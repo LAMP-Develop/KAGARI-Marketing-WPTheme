e99e37e7aedd01353a30a34e2eee4f3affebae20
ShunyaSuzuki <anyushu2017@gmail.com>
2019-12-18 13:21:17 +0900

    npm run fix

--- imagemin.js
+++ imagemin.js
@@ -1,16 +1,19 @@
-const imagemin = require('imagemin');
+const imagemin = require('imagemin-keep-folder');
 const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminSvgo = require('imagemin-svgo');
 
 (async () => {
-  const files = await imagemin(['assets/img/*.{jpg,png,svg}'], {
-    destination: 'lib/images',
+  const files = await imagemin(['assets/img/**/*.{jpg,png,svg}'], {
+    destination: 'lib/images/',
     plugins: [
       imageminSvgo(),
       imageminJpegtran(),
       imageminPngquant()
-    ]
+    ],
+    replaceOutputDir: output => {
+      return output.replace(/img\//, '../lib/images/');
+    }
   });
   console.log(files);
 })();
